a7f0315a0b01ac3e9148420b22b38f1eec144178
almendrags <almendragiannonesuarez@gmail.com>
2022-07-12 23:01:28 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejercicio1DeFacundoGiannone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal dolar;
+            decimal pesosoficial;
+            decimal pesosparalelo;
+            decimal euro;
+
+
+            try
+            {
+                Console.WriteLine("Ingresar la cantidad de dolares con la quiere comprar pesos: ");
+                dolar = Decimal.Parse(Console.ReadLine());
+                pesosoficial = dolar * 230;
+                Console.WriteLine($"Con esa cantidad de dolares puede comprar {pesosoficial} pesos");
+                pesosparalelo = dolar * 280;
+                euro = dolar * 1.17M;
+
+                Console.WriteLine($"Con la misma cantidad de dolares, se pueden comprar {euro} euros y {pesosparalelo} pesos en el mercado paralelo.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Los valores ingresados no son validos.");
+            }
+            
+        }
+    }
+}
